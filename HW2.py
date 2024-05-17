d75cfeb18b5201dcfa1714fee866b35b7f7d301b
PolyElly <Bajd_bi99@student.itstep.org>
2024-05-18 00:00:32 +0300

    2 second attempt

--- HW2.py
+++ HW2.py
@@ -3,14 +3,26 @@ class Student:
     age: int
     group: str
     money: int
+    workhours: int
+    rest = int
 
-    work = 4
-
-    def __init__(self, name, age, group, money):
+    def __init__(self, name, age, group, money, workhours, rest):
         self.name = name
         self.age = age
         self.group = group
         self.money = money
+        self.workhours = workhours
+        self.rest = rest
+
+    def Work(self, money, workhours):
+        if money <= 100:
+            while money < 500:
+                money = money + 10 * workhours
+
+    def Rest(self, money, rest):
+        while rest > 0:
+            money = money - 50
+            rest = rest - 0.5
 
     def printInfo(self):
         print(f'Student: {self.name}, {self.age}, {self.group}, {self.money}')
@@ -19,8 +31,5 @@ class Student:
         return f'Student: {self.name}, {self.age}, {self.group}, {self.money}'
 
 
-student1 = Student(name="PolyElly", age=113, group="C2126", money='work')
+student1 = Student(name="PolyElly", age=113, group="C2126", money=100, workhours=4, rest=6)
 student1.printInfo()
-
-
-#print(student1.getInfo())
